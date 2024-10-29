aeb0a289ecb1bc13544e4d495ff4dcab2d403afe
mccreed13 <a.lapushenko13@gmail.com>
2024-10-29 14:45:05 +0200

    Added sqrt test

--- src/test/java/com/epam/training/student_anton_lapushenko/SqrtTest.java
+++ src/test/java/com/epam/training/student_anton_lapushenko/SqrtTest.java
@@ -1,10 +1,12 @@
 package com.epam.training.student_anton_lapushenko;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SqrtTest extends BaseTest implements DoubleOneArg{
 
@@ -16,5 +18,11 @@ public class SqrtTest extends BaseTest implements DoubleOneArg{
         double result = calculator.sqrt(a);
         double expected = Math.sqrt(a);
         assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Sqrt of negative number")
+    public void sqrtNegativeNumberTest() {
+        assertThrows(NumberFormatException.class, () -> calculator.sqrt(-1));
     }
 }

0ba18c0d4c25b2235f8f3c3669f3d33ff41f7d84
mccreed13 <a.lapushenko13@gmail.com>
2024-10-29 15:48:59 +0200

    Added parallelization

--- src/test/java/com/epam/training/student_anton_lapushenko/BaseTest.java
+++ src/test/java/com/epam/training/student_anton_lapushenko/BaseTest.java
@@ -2,7 +2,10 @@ package com.epam.training.student_anton_lapushenko;
 
 import com.epam.tat.module4.Calculator;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
+@Execution(ExecutionMode.CONCURRENT)
 public abstract class BaseTest {
     protected static Calculator calculator;
 
